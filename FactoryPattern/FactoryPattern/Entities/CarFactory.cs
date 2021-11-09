772e7680c891c665d451f67dab463205ddab0727
corvinus-bj3ksf <magyargeri10@gmail.com>
2021-11-09 23:29:19 +0100

    task09

---
+++ FactoryPattern/FactoryPattern/Entities/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using FactoryPattern.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Entities
+{
+    class CarFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
+    }
+}
