8fde6bbf19e03b62529ca894505a1c265c557d7d
corvinus-bj3ksf <magyargeri10@gmail.com>
2021-11-09 23:25:23 +0100

    task07

---
+++ FactoryPattern/FactoryPattern/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using FactoryPattern.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
