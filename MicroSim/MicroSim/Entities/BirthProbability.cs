a6975b56041bc93405c35643c705cff629d102dc
corvinus-bj3ksf <magyargeri10@gmail.com>
2021-11-29 20:32:32 +0100

    task01

---
+++ MicroSim/MicroSim/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroSim.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int ChildNumbers { get; set; }
+        public double BirthProb { get; set; }
+    }
+}
---
+++ MicroSim/MicroSim/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroSim.Entities
+{
+    public class DeathProbability
+    {
+        public int Gender { get; set; }
+        public int Age { get; set; }
+        public double DeathProb { get; set; }
+    }
+}
